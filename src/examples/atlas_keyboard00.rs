0942f0ebdd92bf5712104c1f202d4df96e11401e
Lightnet <darkneter@gmail.com>
2024-03-04 22:55:11 -0800

    test key but might have error.

--- src/examples/atlas_keyboard00.rs
+++ src/examples/atlas_keyboard00.rs
@@ -78,16 +78,16 @@ fn load_textures(
   mut keyBoards: ResMut<DisplayKeys>,
 ) {
   let boardkeys: Vec<ViewKey> = vec![
-    ViewKey{press_id:0,release_id:1,key:KeyCode::Numpad0,row:0},
-    ViewKey{press_id:2,release_id:3,key:KeyCode::Numpad1,row:0},
-    ViewKey{press_id:4,release_id:5,key:KeyCode::Numpad2,row:0},
-    ViewKey{press_id:6,release_id:7,key:KeyCode::Numpad3,row:0},
-    ViewKey{press_id:8,release_id:9,key:KeyCode::Numpad4,row:0},
-    ViewKey{press_id:10,release_id:11,key:KeyCode::Numpad5,row:0},
-    ViewKey{press_id:12,release_id:13,key:KeyCode::Numpad6,row:0},
-    ViewKey{press_id:14,release_id:15,key:KeyCode::Numpad7,row:0},
-    ViewKey{press_id:16,release_id:17,key:KeyCode::Numpad8,row:0},
-    ViewKey{press_id:18,release_id:19,key:KeyCode::Numpad9,row:0},
+    ViewKey{press_id:0,release_id:1,key:KeyCode::Numpad0,row:1},
+    ViewKey{press_id:2,release_id:3,key:KeyCode::Numpad1,row:1},
+    ViewKey{press_id:4,release_id:5,key:KeyCode::Numpad2,row:1},
+    ViewKey{press_id:6,release_id:7,key:KeyCode::Numpad3,row:1},
+    ViewKey{press_id:8,release_id:9,key:KeyCode::Numpad4,row:1},
+    ViewKey{press_id:10,release_id:11,key:KeyCode::Numpad5,row:1},
+    ViewKey{press_id:12,release_id:13,key:KeyCode::Numpad6,row:1},
+    ViewKey{press_id:14,release_id:15,key:KeyCode::Numpad7,row:1},
+    ViewKey{press_id:16,release_id:17,key:KeyCode::Numpad8,row:1},
+    ViewKey{press_id:18,release_id:19,key:KeyCode::Numpad9,row:1},
 
     //ViewKey{press_id:54,release_id:63,key:KeyCode::Numpad9,row:1},
     //ViewKey{press_id:22,release_id:64,key:KeyCode::Numpad9,row:1},
@@ -141,8 +141,130 @@ fn load_textures(
     //ViewKey{press_id:34,release_id:113,key:KeyCode::Numpad9,row:1},// F6
     //ViewKey{press_id:34,release_id:114,key:KeyCode::Numpad9,row:1},// F7
     //ViewKey{press_id:34,release_id:115,key:KeyCode::Numpad9,row:1},// F7
-    ViewKey{press_id:34,release_id:116,key:KeyCode::Numpad9,row:1},// F8
-    ViewKey{press_id:34,release_id:117,key:KeyCode::Numpad9,row:1},// F8
+    //ViewKey{press_id:34,release_id:116,key:KeyCode::Numpad9,row:1},// F8
+    //ViewKey{press_id:34,release_id:117,key:KeyCode::Numpad9,row:1},// F8
+    //ViewKey{press_id:34,release_id:118,key:KeyCode::Numpad9,row:2},// F9
+    //ViewKey{press_id:34,release_id:119,key:KeyCode::Numpad9,row:2},// F9
+    //ViewKey{press_id:34,release_id:120,key:KeyCode::Numpad9,row:2},// FN
+    //ViewKey{press_id:34,release_id:121,key:KeyCode::Numpad9,row:2},// FN
+    //ViewKey{press_id:34,release_id:122,key:KeyCode::Numpad9,row:2},// F
+    //ViewKey{press_id:34,release_id:123,key:KeyCode::Numpad9,row:2},// G
+    //ViewKey{press_id:34,release_id:124,key:KeyCode::Numpad9,row:2},// G
+    //ViewKey{press_id:34,release_id:125,key:KeyCode::Numpad9,row:2},// H
+    //ViewKey{press_id:34,release_id:126,key:KeyCode::Numpad9,row:2},// Home
+    //ViewKey{press_id:34,release_id:127,key:KeyCode::Numpad9,row:2},// Home
+    //ViewKey{press_id:34,release_id:128,key:KeyCode::Numpad9,row:2},// H
+    //ViewKey{press_id:34,release_id:129,key:KeyCode::Numpad9,row:2},// I
+    //ViewKey{press_id:34,release_id:130,key:KeyCode::Numpad9,row:2},// INS
+    //ViewKey{press_id:34,release_id:131,key:KeyCode::Numpad9,row:2},// INS
+    //ViewKey{press_id:34,release_id:132,key:KeyCode::Numpad9,row:2},// I
+    //ViewKey{press_id:34,release_id:133,key:KeyCode::Numpad9,row:2},// J
+    //ViewKey{press_id:34,release_id:134,key:KeyCode::Numpad9,row:2},// J
+    //ViewKey{press_id:34,release_id:135,key:KeyCode::Numpad9,row:2},// K
+    //ViewKey{press_id:34,release_id:136,key:KeyCode::Numpad9,row:2},// K
+    //ViewKey{press_id:34,release_id:137,key:KeyCode::Numpad9,row:2},// L
+    //ViewKey{press_id:34,release_id:138,key:KeyCode::Numpad9,row:2},// L
+    //ViewKey{press_id:34,release_id:139,key:KeyCode::Numpad9,row:2},// M
+    //ViewKey{press_id:34,release_id:140,key:KeyCode::Numpad9,row:2},// -
+    //ViewKey{press_id:34,release_id:141,key:KeyCode::Numpad9,row:2},// -
+    //ViewKey{press_id:34,release_id:142,key:KeyCode::Numpad9,row:2},// M
+    //ViewKey{press_id:34,release_id:143,key:KeyCode::Numpad9,row:2},// N
+    //ViewKey{press_id:34,release_id:144,key:KeyCode::Numpad9,row:2},// Num Lock
+    //ViewKey{press_id:34,release_id:145,key:KeyCode::Numpad9,row:2},// Num Lock
+    //ViewKey{press_id:34,release_id:146,key:KeyCode::Numpad9,row:2},// Enter
+    //ViewKey{press_id:34,release_id:147,key:KeyCode::Numpad9,row:2},// Enter
+    //ViewKey{press_id:34,release_id:148,key:KeyCode::Numpad9,row:2},// +
+    //ViewKey{press_id:34,release_id:149,key:KeyCode::Numpad9,row:2},// +
+    //ViewKey{press_id:34,release_id:150,key:KeyCode::Numpad9,row:2},// N
+    //ViewKey{press_id:34,release_id:151,key:KeyCode::Numpad9,row:2},// O
+    //ViewKey{press_id:34,release_id:152,key:KeyCode::Numpad9,row:2},// option
+    //ViewKey{press_id:34,release_id:153,key:KeyCode::Numpad9,row:2},// option
+    //ViewKey{press_id:34,release_id:154,key:KeyCode::Numpad9,row:2},// O
+    //ViewKey{press_id:34,release_id:155,key:KeyCode::Numpad9,row:2},// P
+    //ViewKey{press_id:34,release_id:156,key:KeyCode::Numpad9,row:2},// PageDown
+    //ViewKey{press_id:34,release_id:157,key:KeyCode::Numpad9,row:2},// PageDown
+    //ViewKey{press_id:34,release_id:158,key:KeyCode::Numpad9,row:2},// Page Up
+    //ViewKey{press_id:34,release_id:159,key:KeyCode::Numpad9,row:2},// Page Up
+    //ViewKey{press_id:34,release_id:160,key:KeyCode::Numpad9,row:2},// .
+    //ViewKey{press_id:34,release_id:161,key:KeyCode::Numpad9,row:2}, // .
+    //ViewKey{press_id:34,release_id:162,key:KeyCode::Numpad9,row:2}, // +
+    //ViewKey{press_id:34,release_id:163,key:KeyCode::Numpad9,row:2}, // +
+    //ViewKey{press_id:34,release_id:164,key:KeyCode::Numpad9,row:2}, // Prt SCAN
+    //ViewKey{press_id:34,release_id:165,key:KeyCode::Numpad9,row:2}, // Prt SCAN
+    //ViewKey{press_id:34,release_id:166,key:KeyCode::Numpad9,row:2}, // P
+    //ViewKey{press_id:34,release_id:167,key:KeyCode::Numpad9,row:2}, // Q
+    //ViewKey{press_id:34,release_id:168,key:KeyCode::Numpad9,row:2}, // ?
+    //ViewKey{press_id:34,release_id:169,key:KeyCode::Numpad9,row:2}, // ?
+    //ViewKey{press_id:34,release_id:170,key:KeyCode::Numpad9,row:2}, // "
+    //ViewKey{press_id:34,release_id:171,key:KeyCode::Numpad9,row:2}, // "
+    //ViewKey{press_id:34,release_id:172,key:KeyCode::Numpad9,row:2}, // Q
+    //ViewKey{press_id:34,release_id:173,key:KeyCode::Numpad9,row:2}, // R
+    //ViewKey{press_id:34,release_id:174,key:KeyCode::Numpad9,row:2}, // enter
+    //ViewKey{press_id:34,release_id:175,key:KeyCode::Numpad9,row:2}, // enter
+    //ViewKey{press_id:34,release_id:176,key:KeyCode::Numpad9,row:2}, // R
+    //ViewKey{press_id:34,release_id:177,key:KeyCode::Numpad9,row:2}, // S
+    //ViewKey{press_id:34,release_id:178,key:KeyCode::Numpad9,row:2}, // ;
+    //ViewKey{press_id:34,release_id:179,key:KeyCode::Numpad9,row:2}, // ;
+    //ViewKey{press_id:34,release_id:180,key:KeyCode::Numpad9,row:2}, // Shift
+    //ViewKey{press_id:34,release_id:180,key:KeyCode::Numpad9,row:2}, // Shift
+    //ViewKey{press_id:34,release_id:181,key:KeyCode::Numpad9,row:2}, // Shift icon
+    //ViewKey{press_id:34,release_id:182,key:KeyCode::Numpad9,row:2}, // Shift icon
+    //ViewKey{press_id:34,release_id:183,key:KeyCode::Numpad9,row:2}, // Shift
+    //ViewKey{press_id:34,release_id:184,key:KeyCode::Numpad9,row:2}, // \
+    //ViewKey{press_id:34,release_id:185,key:KeyCode::Numpad9,row:2}, // \
+    //ViewKey{press_id:34,release_id:186,key:KeyCode::Numpad9,row:2}, // /
+    //ViewKey{press_id:34,release_id:187,key:KeyCode::Numpad9,row:2}, // /
+    //ViewKey{press_id:34,release_id:188,key:KeyCode::Numpad9,row:2}, // space
+    //ViewKey{press_id:34,release_id:189,key:KeyCode::Numpad9,row:2}, // space
+    //ViewKey{press_id:34,release_id:190,key:KeyCode::Numpad9,row:2}, // S
+    //ViewKey{press_id:34,release_id:191,key:KeyCode::Numpad9,row:2}, // T 
+    //ViewKey{press_id:34,release_id:192,key:KeyCode::Numpad9,row:2}, // ~
+    //ViewKey{press_id:34,release_id:193,key:KeyCode::Numpad9,row:2}, // ~
+    //ViewKey{press_id:34,release_id:194,key:KeyCode::Numpad9,row:2}, // T
+    //ViewKey{press_id:34,release_id:195,key:KeyCode::Numpad9,row:2}, // U
+    //ViewKey{press_id:34,release_id:196,key:KeyCode::Numpad9,row:2}, // U
+    //ViewKey{press_id:34,release_id:197,key:KeyCode::Numpad9,row:2}, // V
+    //ViewKey{press_id:34,release_id:198,key:KeyCode::Numpad9,row:2}, // V
+    //ViewKey{press_id:34,release_id:199,key:KeyCode::Numpad9,row:2}, // W
+    //ViewKey{press_id:34,release_id:200,key:KeyCode::Numpad9,row:2}, // window icon
+    //ViewKey{press_id:34,release_id:201,key:KeyCode::Numpad9,row:2}, // window icon
+    //ViewKey{press_id:34,release_id:202,key:KeyCode::Numpad9,row:2}, // W
+    //ViewKey{press_id:34,release_id:203,key:KeyCode::Numpad9,row:2}, // X
+    //ViewKey{press_id:34,release_id:204,key:KeyCode::Numpad9,row:2}, // X
+    //ViewKey{press_id:34,release_id:205,key:KeyCode::Numpad9,row:2}, // Y
+    //ViewKey{press_id:34,release_id:206,key:KeyCode::Numpad9,row:2}, // Y
+    //ViewKey{press_id:34,release_id:207,key:KeyCode::Numpad9,row:2}, // Z
+    //ViewKey{press_id:34,release_id:208,key:KeyCode::Numpad9,row:2}, // Z
+    //ViewKey{press_id:34,release_id:209,key:KeyCode::Numpad9,row:2}, // comand
+    //ViewKey{press_id:34,release_id:210,key:KeyCode::Numpad9,row:2}, // comand
+    //ViewKey{press_id:34,release_id:211,key:KeyCode::Numpad9,row:2}, // Tab
+    //ViewKey{press_id:34,release_id:212,key:KeyCode::Numpad9,row:2}, // tab icon
+    //ViewKey{press_id:34,release_id:213,key:KeyCode::Numpad9,row:2}, // tab icon
+    //ViewKey{press_id:34,release_id:214,key:KeyCode::Numpad9,row:2}, // Tab
+    //ViewKey{press_id:34,release_id:215,key:KeyCode::Numpad9,row:2}, // mouse 
+    //ViewKey{press_id:34,release_id:216,key:KeyCode::Numpad9,row:2}, // mouse Horizal
+    //ViewKey{press_id:34,release_id:217,key:KeyCode::Numpad9,row:2}, // mouse L
+    //ViewKey{press_id:34,release_id:218,key:KeyCode::Numpad9,row:2}, // mouse L
+    //ViewKey{press_id:34,release_id:219,key:KeyCode::Numpad9,row:2}, // mouse move
+    //ViewKey{press_id:34,release_id:220,key:KeyCode::Numpad9,row:2}, // mouse           outline
+
+    //ViewKey{press_id:34,release_id:221,key:KeyCode::Numpad9,row:2}, // mouse right 
+    //ViewKey{press_id:34,release_id:222,key:KeyCode::Numpad9,row:2}, // mouse right outline
+    //ViewKey{press_id:34,release_id:223,key:KeyCode::Numpad9,row:2}, // mouse middle
+    //ViewKey{press_id:34,release_id:224,key:KeyCode::Numpad9,row:2}, // mouse middle
+    //ViewKey{press_id:34,release_id:225,key:KeyCode::Numpad9,row:2}, // mouse middle scroll down
+    //ViewKey{press_id:34,release_id:226,key:KeyCode::Numpad9,row:2}, // mouse middle scroll
+    //ViewKey{press_id:34,release_id:227,key:KeyCode::Numpad9,row:2}, // mouse middle scroll up
+    //ViewKey{press_id:34,release_id:228,key:KeyCode::Numpad9,row:2}, // mouse middle scroll up outline
+    //ViewKey{press_id:34,release_id:229,key:KeyCode::Numpad9,row:2}, // mouse middle scroll up
+    //ViewKey{press_id:34,release_id:230,key:KeyCode::Numpad9,row:2}, // mouse middle scroll vert
+    ViewKey{press_id:34,release_id:231,key:KeyCode::Numpad9,row:2}, // 
+    ViewKey{press_id:34,release_id:232,key:KeyCode::Numpad9,row:2}, // mouse middle scroll vert
+    //ViewKey{press_id:34,release_id:233,key:KeyCode::Numpad9,row:2}, // over limited display all icon texture
+    
+    
+
+
 
 
   ];
